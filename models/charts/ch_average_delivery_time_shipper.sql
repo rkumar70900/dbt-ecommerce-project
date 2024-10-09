c04f6a4ae1d3cafd5a63427ec8a6e48791287263
Rajesh Kumar Bandaru <54278302+rkumar70900@users.noreply.github.com>
2024-10-09 15:26:24 -0500

    comments on charts

--- models/charts/ch_average_delivery_time_shipper.sql
+++ models/charts/ch_average_delivery_time_shipper.sql
@@ -1,8 +1,15 @@
-with orders as (
-    select *
-    from {{ ref('dt_orders') }}
-)
+"""
+The query uses a subquery called orders to select data from the dt_orders table, which contains information about orders. The main query then calculates the average difference between the carrier delivery date and the approved date for each order, effectively measuring the average delivery time for each shipper.
+"""
 
+
+with orders as (
+    select
+        *
+    from
+        dt_orders
+),
 select
     avg(datediff(carrierDelivery, approvedAt)) as averageShipperDelivery
-from orders
+from
+    orders
